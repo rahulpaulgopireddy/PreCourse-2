1f2cb13fa6633298782ab9b7b3feb037b8062203
rahulgopireddy <rgopired@uncc.edu>
2024-03-31 00:46:17 -0400

    merge sort

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,40 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
-  
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+
+        mergeSort(left_arr)
+        mergeSort(right_arr)
+
+        i = j = k = 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
+  print(arr)
+  return arr
     
     #write your code here
   
